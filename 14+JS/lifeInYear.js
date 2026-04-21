77f077d0891d0788f3cbcdcad2960d7971aed197
Azusa Fukuda <afukuda@mySeneca.ca>
2026-04-21 15:08:04 -0400

    added a file

---
+++ 14+JS/lifeInYear.js
@@ -0,0 +1,12 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var year = 90 - age;
+    var z = year * 12;
+    var y = year * 52;
+    var x = year * 365;
+    console.log("You have " + x + " days, " + y + " weeks, and " +  z + " months left.");
+/*************Don't change the code below**********/
+}
