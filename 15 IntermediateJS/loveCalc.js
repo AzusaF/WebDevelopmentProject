59a81fa9cd55aee49fcd15a44afc9fe7ae238c62
Azusa Fukuda <afukuda@mySeneca.ca>
2026-04-23 14:35:44 -0400

    completed the challenge

---
+++ 15 IntermediateJS/loveCalc.js
@@ -0,0 +1,7 @@
+var yourName = prompt("What is your name?");
+var theirName = prompt("What is their name?");
+
+var rodomNumber = Math.random()*100;
+rodomNumber = Math.floor(rodomNumber) + 1;
+
+alert("Your match: " + rodomNumber + " %");
