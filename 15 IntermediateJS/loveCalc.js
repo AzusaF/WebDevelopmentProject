432b2f5988a3f187e920cbda46ee58ff8dace28e
Azusa Fukuda <afukuda@mySeneca.ca>
2026-04-25 07:41:31 -0400

    added lines

--- 15 IntermediateJS/loveCalc.js
+++ 15 IntermediateJS/loveCalc.js
@@ -3,5 +3,10 @@ var theirName = prompt("What is their name?");
 
 var rodomNumber = Math.random()*100;
 rodomNumber = Math.floor(rodomNumber) + 1;
-
-alert("Your match: " + rodomNumber + " %");
+if (rodomNumber < 30){
+   alert("Your match: " + rodomNumber + " %. You may have a better match!");
+}else if (rodomNumber < 80) {
+   alert("Your match: " + rodomNumber + " %");
+}else {
+   alert("Your match: " + rodomNumber + " %. You two are great together!");
+}
