d977915b1fa0639115e9a5947c9403f91787d911
Azusa Fukuda <afukuda@mySeneca.ca>
2026-04-27 11:59:50 -0400

    added lines

---
+++ 15 IntermediateJS/bmiCalc.js
@@ -0,0 +1,13 @@
+function bmiCalculator (weight, height) {
+    var interpretation;
+    var bmi = Math.round((weight / (height * height))*10)/10;
+    if (bmi < 18.5) {
+      interpretation = "Your BMI is "+ bmi + ", so you are underweight."
+    } else if (bmi >= 18.5 && bmi <= 24.9){
+       interpretation = "Your BMI is "+ bmi + ", so you have a normal weight." 
+    } else {
+        interpretation = "Your BMI is "+ bmi + ", so you are overweight." 
+
+    }
+    return interpretation;
+}
