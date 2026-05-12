a45556766790c71b70985cac70943b9fbc6dff1f
Azusa Fukuda <afukuda@mySeneca.ca>
2026-05-12 13:19:05 -0400

    completed the challenge

---
+++ 15 IntermediateJS/lunch.js
@@ -0,0 +1,11 @@
+function whosPaying(names) {
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    var randomNum = Math.floor(Math.random() * names.length);
+    var person = names[randomNum];
+    
+
+    return person + " is going to buy lunch today!"
+/******Don't change the code below*******/    
+}
