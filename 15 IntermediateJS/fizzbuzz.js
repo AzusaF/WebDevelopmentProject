645f8a09ebbc584a3b5ca3722e8d90d4f38d286d
Azusa Fukuda <afukuda@mySeneca.ca>
2026-05-12 12:29:23 -0400

    completed the challenge

--- 15 IntermediateJS/fizzbuzz.js
+++ 15 IntermediateJS/fizzbuzz.js
@@ -1,6 +1,15 @@
 var output = [];
-
+var count;
 function fizzBuzz(){
-   output.push(output.length+1); 
+   count = output.length+1
+   if (count%15 === 0){
+      output.push("FizzBuzz"); 
+   } else if (count%5 === 0){
+      output.push("Buzz");
+   } else if (count%3 === 0){
+      output.push("Fizz");
+   } else {
+      output.push(count); 
+   }
    console.log(output);
 }
