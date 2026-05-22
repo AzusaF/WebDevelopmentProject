90bb91d085c187a55f289ad6099f91a2d6d9a80c
Azusa Fukuda <afukuda@mySeneca.ca>
2026-05-22 07:16:21 -0400

    one img function

--- 17 Dicee Challenge/index.js
+++ 17 Dicee Challenge/index.js
@@ -1,29 +1,22 @@
 // var randomNumber1 = Math.floor(Math.random()*6+1);
 
+
 function random() {
      return Math.floor(Math.random()*6+1);
 }
 // var random = Math.floor(Math.random()*6);
 
-var num1;
-var num2;
-
-function img1(){
-     num1 = random();
-     string = "./images/dice" + num1 +".png"; 
-     document.querySelectorAll("img")[0].setAttribute("src", string);
+function img(index, num){
+     num = random();
+     string = "./images/dice" + num +".png"; 
+     document.querySelectorAll("img")[index].setAttribute("src", string);
+     return num;
 }
 
-function img2(){
-     num2 = random();
-     string = "./images/dice" + num2 +".png"; 
-     document.querySelectorAll("img")[1].setAttribute("src", string);
-}
-
-function text(){
-     if (num1 === num2){
+function text(n1, n2){
+     if (n1 === n2){
           document.querySelector("h1").innerHTML = "Draw!";
-     } else if (num1 > num2){
+     } else if (n1 > n2){
           document.querySelector("h1").innerHTML = "Player1 Wins!";
      } else {
           document.querySelector("h1").innerHTML = "Player2 Wins!";
@@ -32,9 +25,10 @@ function text(){
 
 
 function onLoad(){
-     img1();
-     img2();
-     text();
+      var num1, num2;
+      num1 = img(0, num1);
+      num2 = img(1, num2);
+      text(num1, num2);
 }
 
 document.querySelector("body").addEventListener("load", onLoad);
