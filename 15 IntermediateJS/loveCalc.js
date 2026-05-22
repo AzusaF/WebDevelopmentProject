d1009221d4daaeaac6ff7bdd1f3fa137926ea1a0
Azusa Fukuda <afukuda@mySeneca.ca>
2026-05-22 06:59:33 -0400

    fixed spelling error

--- 15 IntermediateJS/loveCalc.js
+++ 15 IntermediateJS/loveCalc.js
@@ -1,12 +1,12 @@
 var yourName = prompt("What is your name?");
 var theirName = prompt("What is their name?");
 
-var rodomNumber = Math.random()*100;
-rodomNumber = Math.floor(rodomNumber) + 1;
-if (rodomNumber < 30){
-   alert("Your match: " + rodomNumber + " %. You may have a better match!");
-}else if (rodomNumber < 80) {
-   alert("Your match: " + rodomNumber + " %");
+var radomNumber = Math.random()*100;
+radomNumber = Math.floor(radomNumber) + 1;
+if (radomNumber < 30){
+   alert("Your match: " + radomNumber + " %. You may have a better match!");
+}else if (radomNumber < 80) {
+   alert("Your match: " + radomNumber + " %");
 }else {
-   alert("Your match: " + rodomNumber + " %. You two are great together!");
+   alert("Your match: " + radomNumber + " %. You two are great together!");
 }
