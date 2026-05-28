c48fb06eeeb3f355305a0c6b0bfb7b4d6f996ca8
Azusa Fukuda <afukuda@mySeneca.ca>
2026-05-28 09:33:47 -0400

    challenge 6: add click effect

--- 20 Simon Game/game.js
+++ 20 Simon Game/game.js
@@ -23,9 +23,17 @@ $(".btn").on("click", function(e){
    userClickedPettern.push(userChosenColour);
    console.log(userClickedPettern);
    playSound(userChosenColour);
+   animatePress(userChosenColour);
 });
 
 function playSound(name){
 var audio = new Audio('sounds/' + name + '.mp3');
    audio.play();
+}
+
+function animatePress(currentColour){
+   $("#" + currentColour).addClass("pressed");
+   setTimeout(function(){
+      $("#" + currentColour).removeClass("pressed");
+   }, 100);
 }
