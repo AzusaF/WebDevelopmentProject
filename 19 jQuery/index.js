949b17c74719ba321c154f32c72a88fda809c2f1
Azusa Fukuda <afukuda@mySeneca.ca>
2026-05-27 22:17:31 -0400

    completed the challenge

--- 19 jQuery/index.js
+++ 19 jQuery/index.js
@@ -10,3 +10,14 @@ $("h1").on("click", function(){
       $("h1").attr("class", "already-pressed");
    }, 500)
 });
+
+$("button").on("click", function(){
+   $("h1").slideToggle();
+   });
+
+$("h1").on("mouseover", function(){
+   $("h1").animate({opacity: 0.5});
+   setTimeout(function(){
+       $("h1").animate({opacity: 1});
+   }, 700)
+});
