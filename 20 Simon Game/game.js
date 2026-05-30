5422f7e5cadb5f678e94960e65bbb0ea781ae1a8
Azusa Fukuda <afukuda@mySeneca.ca>
2026-05-30 09:39:18 -0400

    completed the challenge

--- 20 Simon Game/game.js
+++ 20 Simon Game/game.js
@@ -1,33 +1,42 @@
-// 1. Inside game.js create a new function called nextSequence()
-// 2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
-// You can use the Chrome console to verify that your code creates random numbers between the correct range.
-// 3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
-// 4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
-// 5. At the top of the game.js file, create a new empty array called gamePattern.
-// 6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
-
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPettern = [];
 
+var pressedKeys = [];
+
+var level = 0;
+var started = false;
+
+// next level
 function nextSequence(){
+   userClickedPettern = [];
+   level++;
+   $("h1").text("Level " +  level);
+
    var randomNumber = Math.floor(Math.random()*4);
    var randomChosenColour = buttonColours[randomNumber];
+   
    gamePattern.push(randomChosenColour);
+   
    $("#" + randomChosenColour).animate({opacity: 0}, 100).animate({opacity: 100}, 100);
    playSound(randomChosenColour);
 }
 
+// animation and sound
 $(".btn").on("click", function(e){
    var userChosenColour = $(this).attr("id");
    userClickedPettern.push(userChosenColour);
+
    console.log(userClickedPettern);
+   
    playSound(userChosenColour);
    animatePress(userChosenColour);
+   checkAnswer(userChosenColour.length-1);
 });
 
 function playSound(name){
-var audio = new Audio('sounds/' + name + '.mp3');
+   var audio = new Audio('sounds/' + name + '.mp3');
    audio.play();
 }
 
@@ -36,4 +45,42 @@ function animatePress(currentColour){
    setTimeout(function(){
       $("#" + currentColour).removeClass("pressed");
    }, 100);
+}
+
+
+// initial key down
+$(document).on("keydown", function(e){
+   console.log(e.key);
+   if (!started){
+      $("h1").text("Level " +  level);
+      nextSequence();
+      started = true;
+   }
+   pressedKeys.push(e.key);
+});
+
+function checkAnswer(index){
+   if (userClickedPettern[index] === gamePattern[index]){
+      console.log("correct");
+      if (userClickedPettern.length === gamePattern.length){
+         setTimeout(nextSequence(), 1000)
+      } 
+   } else {
+      playSound("wrong");
+
+      $("body").addClass("game-over");
+      setTimeout(function(){
+         $("body").removeClass("game-over");
+      }, 200)
+
+      $("h1").text("Game Over, Press Any Key to Restart");
+      startOver();
+   }
+}
+
+function startOver(){
+   level = 0;
+   gamePattern = [];
+   started = false;
+   pressedKeys = [];
 }
