b85e5cbbd5b741b54d2de2638d2f08975c4dfc55
Azusa Fukuda <afukuda@mySeneca.ca>
2026-06-01 13:29:54 -0400

    challenge completed

--- 23 Node/2.3 NPM/index.js
+++ 23 Node/2.3 NPM/index.js
@@ -1,4 +1,7 @@
 import generateName from "sillyname";
 var sillyName = generateName();
 
-console.log(`My name is ${sillyName}.`)
+import {randomSuperhero}  from "superheroes";
+const newSuperhero = randomSuperhero();
+
+console.log(`My name is ${sillyName}. I am ${newSuperhero}!`);
