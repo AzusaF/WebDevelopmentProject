6c864245efdef87ca747ffa16dd1f60a6391d925
Azusa Fukuda <afukuda@mySeneca.ca>
2026-07-17 15:27:48 -0400

    complete challenge

--- 33 PostgreSQL/World Capital Quiz/index.js
+++ 33 PostgreSQL/World Capital Quiz/index.js
@@ -1,14 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
 
+dotenv.config({path: "./secret.env"});
+
+const dbpassword = process.env.PASSWORD;
+
+const db = new pg.Client({
+  user:"postgres",
+  host:"localhost",
+  database: "world",
+  password: dbpassword,
+  port:5432,
+});
+
+db.connect();
+
 let quiz = [
   { country: "France", capital: "Paris" },
   { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
+  { country: "United States of America", capital: "New York" }, //Wasington DC
 ];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
