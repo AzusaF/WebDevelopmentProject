a3d8822ed671c1bce90b44c800bb4a3f6675c133
Azusa Fukuda <afukuda@mySeneca.ca>
2026-07-23 15:20:58 -0400

    complete additional challenges

--- 33 PostgreSQL/8.3 Travel Tracker/index.js
+++ 33 PostgreSQL/8.3 Travel Tracker/index.js
@@ -17,9 +17,15 @@ const db = new pg.Client({
 
 db.connect();
 
-function capitalize(str) {
-  if (!str) return "";
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+async function checkVisited(){
+  var countries = [];
+  try {
+    const result = await db.query("SELECT country_code FROM visited_countries");
+    result.rows.forEach((country) => countries.push(country.country_code));
+  } catch (err) {
+    console.error("Error executing query", err.stack);
+  }
+  return countries;
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,13 +33,7 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
-  var countries_to_map = [];
-  try {
-    const result = await db.query("SELECT country_code FROM visited_countries");
-    result.rows.forEach((country) => countries_to_map.push(country.country_code));
-  } catch (err) {
-    console.error("Error executing query", err.stack);
-  }
+  const countries_to_map = await checkVisited();
   res.render("index.ejs", { 
     countries: countries_to_map,
     total: countries_to_map.length
@@ -41,12 +41,34 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/add", async (req, res) => {
-  const country_name =  capitalize(req.body.country);
-  const result = await db.query("SELECT country_code FROM countries WHERE country_name = $1", [country_name]);
-  const data = result.rows[0];
-  const country_code = data.country_code;
-  await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [country_code]);
-  res.redirect("/")
+  const country_name =  req.body.country;
+  try {
+    const result = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE  '%' || $1 || '%';", [country_name.toLowerCase()]);
+    const data = result.rows[0];
+    const country_code = data.country_code;
+    try {
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [country_code]);
+      res.redirect("/")
+    } catch (err) {
+      var message = "You already added the country. Try again.";
+      console.log(err);
+      const countries_to_map = await checkVisited();
+      res.render("index.ejs", { 
+        countries: countries_to_map,
+        total: countries_to_map.length,
+        error:  message
+      });
+    }
+  } catch (err) {
+    var message = "We couldn't find the country. Try again.";
+    console.log(err);
+    const countries_to_map = await checkVisited();
+    res.render("index.ejs", { 
+      countries: countries_to_map,
+      total: countries_to_map.length,
+      error:  message
+    });
+  }
 });
 
 app.listen(port, () => {
